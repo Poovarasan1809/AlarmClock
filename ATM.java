d00cad1667159cff2ef3837497cdf3cb781b9b27
POOVARASAN S <150900265+Poovarasan1809@users.noreply.github.com>
2024-01-08 21:03:42 +0530

    ATM.java

--- ATM.java
+++ ATM.java
@@ -20,35 +20,35 @@ public class ATMExample
             switch(choice)  
             {  
                 case 1:  
-        	System.out.print("Enter money to be withdrawn:");  
-        	withdraw = sc.nextInt();  
-        	if(balance >= withdraw)  
-       	{  
-	   balance = balance - withdraw;  
-          	   System.out.println("Please collect your money");  
-       	 }  
-        	else  
-        	{   
-            	  System.out.println("Insufficient Balance");  
-        	}  
-        	System.out.println("");  
-        	break;  
+        		System.out.print("Enter money to be withdrawn:");  
+        		withdraw = sc.nextInt();  
+        		if(balance >= withdraw)  
+       			{  
+	   			balance = balance - withdraw;  
+          	   		System.out.println("Please collect your money");  
+       	 		}  
+        		else  
+        		{   
+            	  		System.out.println("Insufficient Balance");  
+        		}  
+        		System.out.println("");  
+        		break;  
    
                 case 2:  
-                    System.out.print("Enter money to be deposited:");  
-                    deposit = sc.nextInt();  
-                    balance = balance + deposit;  
-        	System.out.println("Your Money has been successfully depsited");  
-        	System.out.println("");  
-        	break;  
+                    	System.out.print("Enter money to be deposited:");  
+                    	deposit = sc.nextInt();  
+                    	balance = balance + deposit;  
+        		System.out.println("Your Money has been successfully depsited");  
+        		System.out.println("");  
+        		break;  
    
                 case 3:  
-	System.out.println("Balance : "+balance);  
-        	System.out.println("");  
-        	break;  
+			System.out.println("Balance : "+balance);  
+        		System.out.println("");  
+        		break;  
    
                 case 4:  
-        	System.exit(0);  
+        		System.exit(0);  
             }  
         }  
     }  
